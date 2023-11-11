c97322b9735af08e9f799ee0b0d3bd062b72d6a0
natsu0102 <nms08018688288@icloud.com>
2023-11-11 18:50:52 +0900

    ブログ投稿処理実行

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use App\Http\Controllers\PostController;
 |
 */
 Route::get('/', [PostController::class, 'index']); 
+Route::get('/posts/create', [PostController::class,'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメゾッドを実行する
+Route::post('/posts', [PostController::class, 'store']);
 ?>
+
